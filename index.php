ea9c13c3c5809bde7c8cf999203c4dafa31189b9
Debjit <debjitlm10@gmail.com>
2022-02-19 13:00:39 +0530

    committed my proj

---
+++ index.php
@@ -0,0 +1,31 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <title>Banking System</title>
+  </head>
+
+  <body >
+  <header>
+  <?php
+  include 'navbar.php';
+  ?></header>
+
+  <body>
+    <div class="home">
+      <div class="container">
+        <h1>Welcome to Sparks Bank</h1>
+        <h3>The Most Trusted Bank of India</h3>
+      </div>
+    </div>
+    
+    <footer class="center">
+      Sparks Bank Website | copyright &copy Debjit Roy | All Rights Reserved
+    </footer>
+  </body>
+  
+</html>
